cc3e5d2efdad2bf2333945b7a626e21aedffc72e
Taejoon Kwon <linusben@gmail.com>
2014-06-09 23:29:04 -0500

    Revised fetcher for legislative/local data

--- nec.go.kr/wget-nec_go_kr-level_1.py
+++ nec.go.kr/wget-nec_go_kr-level_1.py
@@ -6,7 +6,7 @@ import urllib
 import time
 from BeautifulSoup import BeautifulSoup
 
-filename_list = 'election_info.presidential.txt'
+filename_list = 'election_info.local.txt'
 
 election_title = filename_list.split('.')[1]
 
@@ -22,10 +22,13 @@ for line in f_list:
     elections[ tmp_election_name ] = {'type':tmp_election_type, 'code':tmp_election_code}
 f_list.close()
 
-## Variables
-## $election_type, $election_code, $city_code
-#url_template = string.Template('http://info.nec.go.kr/electioninfo/electionInfo_report.xhtml?electionId=0000000000&requestURI=%2Felectioninfo%2F0000000000%2Fvc%2Fvccp09.jsp&statementId=VCCP02_%231&electionName=$election_code&electionCode=$election_type&cityCode=$city_code')
-url_template = string.Template('http://info.nec.go.kr/electioninfo/electionInfo_report.xhtml?electionId=0000000000&requestURI=%2Felectioninfo%2F0000000000%2Fvc%2Fvccp09.jsp&topMenuId=VC&secondMenuId=VCCP&menuId=VCCP09&statementId=VCCP02_%2391&oldElectionType=0&electionType=$election_type&electionName=$election_code&electionCode=$election_type&cityCode=$city_code')
+## Variables - $election_type, $election_code, $city_code
+
+## For presidential 
+#url_template = string.Template('http://info.nec.go.kr/electioninfo/electionInfo_report.xhtml?electionId=0000000000&requestURI=%2Felectioninfo%2F0000000000%2Fvc%2Fvccp09.jsp&topMenuId=VC&secondMenuId=VCCP&menuId=VCCP09&statementId=VCCP02_%2391&oldElectionType=0&electionType=$election_type&electionName=$election_code&electionCode=$election_type&cityCode=$city_code')
+
+## For legislative
+url_template = string.Template('http://info.nec.go.kr/electioninfo/electionInfo_report.xhtml?electionId=0000000000&requestURI=%2Felectioninfo%2F0000000000%2Fvc%2Fvccp09.jsp&topMenuId=VC&secondMenuId=VCCP&menuId=VCCP09&statementId=VCCP02_%2391&oldElectionType=&electionType=$election_type&electionName=$election_code&electionCode=$election_type&cityCode=$city_code')
 
 city_code_list = []
 f_region = open('city_town_code.txt','r')
@@ -37,17 +40,14 @@ for line in f_region:
 f_region.close()
 
 for tmp_name in sorted(elections.keys(),reverse=True):
-    if( not tmp_name.startswith('presidential.20') ):
-        continue
-
     for city_code in list(set(city_code_list)):
-        ## Before 2007
         city_code = city_code.replace('00','')
 
         tmp_e_type = elections[tmp_name]['type']
         tmp_e_code = elections[tmp_name]['code']
         tmp_var = {'election_code':tmp_e_code, 'election_type':tmp_e_type,'city_code':city_code}
         tmp_url = url_template.substitute(tmp_var)
+        sys.stderr.write('URL: %s\n'%tmp_url)
         f_url = urllib.urlopen(tmp_url)
         tmp_content = f_url.read()
         f_url.close()

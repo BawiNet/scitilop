630a68d73a7b31799c3136df7c46f71ebd046044
Taejoon Kwon <linusben@gmail.com>
2014-06-28 17:29:31 -0500

    Download kostat data (2000-2012)

--- kostat.go.kr/geojson-list-code.py
+++ kostat.go.kr/geojson-list-code.py
@@ -5,15 +5,26 @@ import string
 import urllib
 import json
 import time
+import gzip
 from pprint import pprint
 
 filename_geojson = sys.argv[1]
 
-json_data = open(filename_geojson)
+json_data = open(filename_geojson,'r')
+if( filename_geojson.endswith('gz') ):
+    json_data = gzip.open(filename_geojson,'rb')
+
 data = json.load(json_data)
 for tmp in data['features']:
-    #print tmp.keys()
-    tmp_code = tmp['properties']['Name']
-    tmp_unit_name = tmp['properties']['Description']
-    print "%s\t%s"%(tmp_code, tmp_unit_name)
+    #print tmp.keys(), tmp['properties'].keys()
+    if( tmp['properties'].has_key('Name') ):
+        tmp_code = tmp['properties']['Name']
+    elif( tmp['properties'].has_key('admcode') ):
+        tmp_code = tmp['properties']['admcode']
+    
+    if( tmp['properties'].has_key('Description') ):
+        tmp_unit_name = tmp['properties']['Description']
+    elif( tmp['properties'].has_key('admname') ):
+        tmp_unit_name = tmp['properties']['admname']
+    print tmp_code.encode('utf-8')+"\t"+tmp_unit_name.encode('utf-8')
 json_data.close()
--- kostat.go.kr/wget-kostat.go.kr-geojson.py
+++ kostat.go.kr/wget-kostat.go.kr-geojson.py
@@ -5,37 +5,59 @@ import string
 import urllib
 import json
 import time
+import gzip
 
 f_key = open('KEY','r')
 api_key = f_key.readline().strip()
 f_key.close()
 
 ## latest - $key, $code, $step
-query_year='2014'
+#query_year='2014'
+#query_step = 2
+#url_template = string.Template('http://sgis.kostat.go.kr/OpenAPI2/adminUnitBoundary.do?apikey=$key&format=geojson&code=$code&step=$step')
+
+## old - $key, $code
+query_year='1980'
 query_step = 2
-url_template = string.Template('http://sgis.kostat.go.kr/OpenAPI2/adminUnitBoundary.do?apikey=$key&format=geojson&code=$code&step=$step')
+url_template = string.Template('http://sgis.kostat.go.kr/OpenAPI2/adminBoundaryByCYL.do?apikey=$key&format=geojson&code=$code&year=$year&level=LEVEL_00')
 
-## old
-#url_template = string.Template('http://sgis.kostat.go.kr/OpenAPI2/adminBoundaryByCYL.do?apikey=$key&format=geojson&code=$code&year=$year&level=LEVEL_00')
+f_list = open('code_list.00.txt','r')
+for line in f_list:
+    tokens = line.strip().split("\t")
+    tmp_code = tokens[0]
+    if( tmp_code != '39' ):
+        continue
+    tmp_unit_name = tokens[1]
+#json_data = open('unit_boundary.2014.00.1.geojson')
+#data = json.load(json_data)
+#for tmp in data['features']:
+#    tmp_code = tmp['properties']['Name']
+#    tmp_unit_name = tmp['properties']['Description']
 
-json_data = open('unit_boundary.2014.00.1.geojson')
-data = json.load(json_data)
-for tmp in data['features']:
-    tmp_code = tmp['properties']['Name']
-    tmp_unit_name = tmp['properties']['Description']
-    tmp_var = {'key':api_key, 'code':tmp_code, 'step':query_step}
-
-    tmp_url = url_template.substitute(tmp_var)
-    sys.stderr.write('Name: %s, URL: %s\n'%(tmp_unit_name,tmp_url))
-
-    f_url = urllib.urlopen(tmp_url)
-    tmp_content = f_url.read()
-    f_url.close()
+    json_2014 = gzip.open('geojson/unit_boundary.2014.%s.%s.geojson.gz'%(tmp_code,query_step),'rb')
+    data_2014 = json.load(json_2014)
+    tmp_out = {'type':'FeatureCollections','features':[]}
     filename_out = 'unit_boundary.%s.%s.%d.geojson'%(query_year,tmp_code,query_step)
+    for tmp in data_2014['features']:
+        tmp_code2 = tmp['properties']['Name']
+        tmp_unit_name2 = tmp['properties']['Description']
+
+        tmp_var = {'key':api_key, 'year':query_year, 'code':tmp_code2}
+        tmp_url = url_template.substitute(tmp_var)
+        sys.stderr.write("URL:%s\n"%tmp_url)
+
+        f_url = urllib.urlopen(tmp_url)
+        #tmp_content = f_url.read()
+        tmp_data = json.load(f_url)
+        f_url.close()
+        time.sleep(3)
+
+        tmp_out['features'] += tmp_data['features'] 
+
     sys.stderr.write('Write %s ... '%filename_out)
     f_out = open(filename_out,'w')
-    f_out.write(tmp_content)
+    json.dump(tmp_out, f_out)
     f_out.close()
     sys.stderr.write('Done\n')
-    time.sleep(3)
-json_data.close()
+    sys.exit(1)
+#json_data.close()
